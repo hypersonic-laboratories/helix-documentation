cd6743528d19f4aa5887c426fbfe84b117d48cc3
conqr2 <34463817+conqr2@users.noreply.github.com>
2025-06-27 23:52:07 +0800

    Sync banner from production

--- docs/_extra/javascript/extra.js
+++ docs/_extra/javascript/extra.js
@@ -1,20 +1,53 @@
-// Add left/right buttons to navigate pages
-keyboard$
-    .subscribe(key => {
-        // Do nothing when "search" active
-        if (key.mode === "search")
-            return;
-        
-        switch (key.type) {
-            case "ArrowLeft":
-                const prev = document.querySelector("link[rel=prev]");
-                if (typeof prev !== "undefined")
-                    location.href = prev.href;
-                break;
-            case "ArrowRight":
-                const next = document.querySelector("link[rel=next]");
-                if (typeof next !== "undefined")
-                    location.href = next.href;
-                break;
-        }
-    });
+keyboard$.subscribe((key) => {
+  // Do nothing when "search" active
+  if (key.mode === "search") return;
+
+  switch (key.type) {
+    case "ArrowLeft":
+      const prev = document.querySelector("link[rel=prev]");
+      if (typeof prev !== "undefined" && prev !== null)
+        location.href = prev.href;
+      break;
+    case "ArrowRight":
+      const next = document.querySelector("link[rel=next]");
+      if (typeof next !== "undefined" && next !== null)
+        location.href = next.href;
+      break;
+  }
+});
+
+(() => {
+  const docsNotice = localStorage.getItem("docs-notice");
+  if (docsNotice === "hide") return;
+
+  const modalWrapper = (() => {
+    const div = document.createElement("div");
+    div.classList.add("modal-wrapper");
+    document.body.appendChild(div);
+    return div;
+  })();
+
+  const modal = (() => {
+    const div = document.createElement("div");
+    div.classList.add("modal");
+    modalWrapper.appendChild(div);
+    return div;
+  })();
+
+  const button = (() => {
+    const p = document.createElement("p");
+    p.innerHTML =
+      "This documentation is a work in progress. Information may be out of date or inaccurate.";
+    modal.appendChild(p);
+
+    const button = document.createElement("button");
+    button.innerHTML = "Close";
+    modal.appendChild(button);
+    return button;
+  })();
+
+  button.addEventListener("click", () => {
+    modalWrapper.remove();
+    localStorage.setItem("docs-notice", "hide");
+  });
+})();
